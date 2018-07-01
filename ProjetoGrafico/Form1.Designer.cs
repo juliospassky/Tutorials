4c3f9a2ea6ac1e81b14de88493be964b2f62faa1
Júlio César Teixeira de Oliveira <julio@ethosconsultores.com.br>
2018-07-01 20:18:51 -0300

    [1.0.0 Projeto com Gráfico] Criação de um Windows Forms Application utilizando Zed Graph.

---
+++ ProjetoGrafico/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ProjetoGrafico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zed = new ZedGraph.ZedGraphControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // zed
+            // 
+            this.zed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zed.Location = new System.Drawing.Point(208, 46);
+            this.zed.Name = "zed";
+            this.zed.ScrollGrace = 0D;
+            this.zed.ScrollMaxX = 0D;
+            this.zed.ScrollMaxY = 0D;
+            this.zed.ScrollMaxY2 = 0D;
+            this.zed.ScrollMinX = 0D;
+            this.zed.ScrollMinY = 0D;
+            this.zed.ScrollMinY2 = 0D;
+            this.zed.Size = new System.Drawing.Size(325, 190);
+            this.zed.TabIndex = 0;
+            this.zed.UseExtendedPrintDialog = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(545, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(12, 46);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(100, 20);
+            this.txtEntrada.TabIndex = 2;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(12, 72);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 248);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.zed);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zed;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ ProjetoGrafico/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoGrafico
+{
+    public partial class Form1 : Form
+    {
+        static int tamanho = 10;
+        double[] x = new double[tamanho];
+        double[] y = new double[tamanho];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            zed.GraphPane.CurveList.Clear();
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                x[i] = i;
+                y[i] = double.Parse(Microsoft.JScript.Eval.JScriptEvaluate(txtEntrada.Text.Replace("x", i.ToString()),
+               Microsoft.JScript.Vsa.VsaEngine.CreateEngine()).ToString());
+
+
+            }
+
+
+            zed.GraphPane.AddCurve("minha curva", x, y, Color.Black);
+            zed.RestoreScale(zed.GraphPane);
+            zed.Refresh();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog ofd = new OpenFileDialog();
+            //ofd.Title = "Arquivo de texto";
+            //ofd.Filter = "TXT files|*.txt";
+            //ofd.InitialDirectory = Application.StartupPath;
+            //if (ofd.ShowDialog() == DialogResult.OK)
+            //{
+            //    string textoArquivo = System.IO.File.ReadAllText(ofd.FileName);
+
+            //    string[] separadorPorEnter = new string[] { "\r\n" };
+            //    string[] textoEnter = textoArquivo.Split(separadorPorEnter, StringSplitOptions.None);
+
+            //    for (int i = 0; i < tamanho; i++)
+            //    {
+            //        string[] separadorPorEspaco = textoEnter[i].Split(' ');
+            //        x[i] = double.Parse(separadorPorEspaco[0]);
+            //        y[i] = double.Parse(separadorPorEspaco[1]);
+            //    }
+            //}
+        }
+    }
+}
